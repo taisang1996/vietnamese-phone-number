76f94ddb8f06fd4840bb091c49b87b911cba3b84
Tai Sang <taisang1996@gmail.com>
2015-09-06 01:07:34 +0700

    first commit

---
+++ generate-crunch-mobile-number.sh
@@ -0,0 +1,44 @@
+#mobifone
+crunch 10 10 -o mobifone-090-10-digit.txt -t 090%%%%%%%
+crunch 10 10 -o mobifone-093-10-digit.txt -t 093%%%%%%%
+
+crunch 11 11 -o mobifone-0120-11-digit.txt -t 0120%%%%%%%
+crunch 11 11 -o mobifone-0121-11-digit.txt -t 0121%%%%%%%
+crunch 11 11 -o mobifone-0122-11-digit.txt -t 0122%%%%%%%
+crunch 11 11 -o mobifone-0126-11-digit.txt -t 0126%%%%%%%
+crunch 11 11 -o mobifone-0128-11-digit.txt -t 0128%%%%%%%
+
+#vinaphone
+crunch 10 10 -o vinaphone-091-10-digit.txt -t 091%%%%%%%
+crunch 10 10 -o vinaphone-093-10-digit.txt -t 094%%%%%%%
+
+crunch 11 11 -o vinaphone-0123-11-digit.txt -t 0123%%%%%%%
+crunch 11 11 -o vinaphone-0124-11-digit.txt -t 0124%%%%%%%
+crunch 11 11 -o vinaphone-0125-11-digit.txt -t 0125%%%%%%%
+crunch 11 11 -o vinaphone-0127-11-digit.txt -t 0127%%%%%%%
+crunch 11 11 -o vinaphone-0129-11-digit.txt -t 0129%%%%%%%
+
+#viettel
+crunch 10 10 -o viettel-096-10-digit.txt -t 096%%%%%%%
+crunch 10 10 -o viettel-097-10-digit.txt -t 097%%%%%%%
+crunch 10 10 -o viettel-098-10-digit.txt -t 098%%%%%%%
+
+crunch 11 11 -o viettel-0162-11-digit.txt -t 0162%%%%%%%
+crunch 11 11 -o viettel-0163-11-digit.txt -t 0163%%%%%%%
+crunch 11 11 -o viettel-0164-11-digit.txt -t 0164%%%%%%%
+crunch 11 11 -o viettel-0165-11-digit.txt -t 0165%%%%%%%
+crunch 11 11 -o viettel-0166-11-digit.txt -t 0166%%%%%%%
+crunch 11 11 -o viettel-0167-11-digit.txt -t 0167%%%%%%%
+crunch 11 11 -o viettel-0168-11-digit.txt -t 0168%%%%%%%
+crunch 11 11 -o viettel-0169-11-digit.txt -t 0169%%%%%%%
+
+#vietnamobile
+crunch 10 10 -o vietnamobile-092-10-digit.txt -t 092%%%%%%%
+
+crunch 11 11 -o vietnamobile-0186-11-digit.txt -t 0186%%%%%%%
+crunch 11 11 -o vietnamobile-0188-11-digit.txt -t 0188%%%%%%%
+
+#gmobile
+#i'm lazy :D
+
+
